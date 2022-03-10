65d34f7dfbe42e62653fbd20f7470fdce8aa5917
LLPROG <94935580+LLPROG@users.noreply.github.com>
2022-03-10 15:33:40 +0000

    add text to html

---
+++ js/script.js
@@ -0,0 +1,44 @@
+
+/*
+
+Descrizione:
+
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus:
+
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+*/
+
+const settings = {
+	el: '#app', //container dove Vue agisce
+	data: {
+		///dati visualizzati da Vue
+        titleClass: 'title-class',
+        btnClass: 'btn-class',
+        titleMessage: 'ciao',
+        buttonMessage: 'cambia ciao in hello',
+
+        color:'blue'
+
+	},
+	methods: {
+		changeMessage: function() {
+            if(this.titleMessage == 'ciao') {
+                this.titleMessage = 'hello';
+                this.buttonMessage = 'change hello in ciao';
+            } else {
+                this.titleMessage = 'ciao';
+                this.buttonMessage = 'cambia ciao in hello';
+
+
+            }
+            
+		}
+
+	}
+}
+
+const app = new Vue(settings);
+
